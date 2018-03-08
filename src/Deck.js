a2f19b05ff9fdfb6d797e34ae08a606017f7961a
xants <kairey@gmail.com>
2018-03-08 13:42:02 -0800

    individual card moving

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
-import { View, Animated } from "react-native";
+import { View, Animated, PanResponder } from "react-native";
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            style={this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
